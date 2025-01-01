da75eb6c800e26ada415011ba82c91616b0c39d0
syakil <syakilahmadginanjar@gmail.com>
2025-01-01 15:42:53 +0700

    first commit

---
+++ client/src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {}
+
+
+const Dashboard = () => {
+    return (
+        <div>
+            Dashboard
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ client/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import DashboardWrapper from "./dashboardWrapper";
+
+const inter = Inter({subsets: ["latin"]});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <DashboardWrapper>  
+        {children}
+        </DashboardWrapper>    
+      </body>
+    </html>
+  );
+}
